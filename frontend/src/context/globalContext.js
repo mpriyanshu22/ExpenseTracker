2a4c0a77f6ce9cb87248e7206b62b506461af7cd
mpriyanshu22 <iitianki22@gmail.com>
2026-03-03 15:49:20 +0530

    refactor

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'https://expensetracker-backend-0nrd.onrender.com/api/v1/';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 
 axios.defaults.withCredentials = true;
